6aace501353f60abe5e0bb92116c0b3b1d1d2ac7
Carlos Eduardo <srdindong@gmail.com>
2025-03-16 02:03:22 -0300

    very-important-save

--- Aula_1/resolucao3/script.js
+++ Aula_1/resolucao3/script.js
@@ -29,3 +29,67 @@ function typeWriter() {
 }
 
 typeWriter(); // Inicia a animação
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Seleciona o botão dropdown
+    const dropdownButton = document.getElementById('dropdownButton');
+    
+    // Seleciona todos os itens do dropdown
+    const dropdownItems = document.querySelectorAll('.dropdown-item');
+
+    // Adiciona um evento de clique para cada item do dropdown
+    dropdownItems.forEach(item => {
+        item.addEventListener('click', function(event) {
+            event.preventDefault(); // Evita o comportamento padrão do link
+
+            // Obtém o valor do item clicado (Mensal ou Anual)
+            const selectedValue = item.getAttribute('data-value');
+
+            // Verifica se o valor foi capturado corretamente
+            if (selectedValue) {
+                // Atualiza o texto do botão com o valor selecionado
+                dropdownButton.textContent = `% ${selectedValue}`;
+            } else {
+                console.error('Valor não encontrado no item do dropdown.');
+            }
+        });
+    });
+});
+
+//Botões de + e -
+document.querySelector('.minus').addEventListener('click', function() {
+    let input = document.querySelector('.parcelas input');
+    let value = parseInt(input.value);
+    if (value > 1) {
+        input.value = value - 1;
+    }
+});
+
+document.querySelector('.plus').addEventListener('click', function() {
+    let input = document.querySelector('.parcelas input');
+    let value = parseInt(input.value);
+    input.value = value + 1;
+});
+
+//Input de inserir valor
+document.getElementById('valorInput').addEventListener('input', function(event) {
+    let input = event.target;
+    let valor = input.value.replace(/[^0-9]/g, ''); // Remove tudo que não é número
+
+    // Se o valor estiver vazio, define como "0,00"
+    if (valor === '') {
+        input.value = 'R$ 0,00';
+        return;
+    }
+
+    // Garante que o valor tenha no máximo 2 casas decimais
+    let valorFormatado = (Number(valor) / 100).toLocaleString('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
+    });
+
+    // Atualiza o valor do campo de entrada
+    input.value = valorFormatado;
+});
